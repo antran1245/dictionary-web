9e2ce5de1b4fbd57223ae8d7fdfff11515c1d6e3
antran1245 <antran1245@gmail.com>
2023-01-24 21:57:16 -0800

    html head

---
+++ pages/Heading.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Heading() {
+  return (
+    <div>
+      <Head>
+        <title>Dictionary Web</title>
+        <link rel="icon" href="/favicon-32x32.png" />
+      </Head>
+    </div>
+  )
+}
