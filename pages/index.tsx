993c55ce18b6d3692729cf2046e6d88af4f2adb2
antran1245 <antran1245@gmail.com>
2023-01-25 23:32:13 -0800

    getting started with the setting bar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,21 @@
 import Heading from './Heading'
 import styles from '../styles/Home.module.css'
-
+import logo from '../assets/images/logo.svg'
+import Image from 'next/image'
 export default function Home() {
   return (
     <div className={styles.container}>
       <Heading />
+      <div id='settingBar'>
+        <Image src={logo} alt="logo" width={32} height={36.5} />
+        <div id='setting'>
+          <select>
+            <option value="">Sans Serif</option>
+            <option value="">Serif</option>
+            <option value="">Mono</option>
+          </select>
+        </div>
+      </div>
     </div>
   )
 }
