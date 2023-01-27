4a06dc0497d2fedc6b244e6efcc7c992fe87ace0
antran1245 <antran1245@gmail.com>
2023-01-26 19:19:40 -0800

    remove some css and working on the page setting

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 import Heading from './Heading'
-import styles from '../styles/Home.module.css'
 import logo from '../assets/images/logo.svg'
+import moon from '../assets/images/icon-moon.svg'
 import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -9,11 +11,21 @@ export default function Home() {
       <div id='settingBar'>
         <Image src={logo} alt="logo" width={32} height={36.5} />
         <div id='setting'>
-          <select>
-            <option value="">Sans Serif</option>
-            <option value="">Serif</option>
-            <option value="">Mono</option>
-          </select>
+          <div className={styles.dropdown}>
+            <select>
+              <option value="">Sans Serif</option>
+              <option value="">Serif</option>
+              <option value="">Mono</option>
+            </select>
+            <span>&#x2304;</span>
+          </div>
+          <div>
+            <label className={styles.switch}>
+              <input type="checkbox" />
+              <span className={styles.slider}></span>
+            </label>
+            <Image src={moon} alt="moon" width={20} height={20} />
+          </div>
         </div>
       </div>
     </div>
