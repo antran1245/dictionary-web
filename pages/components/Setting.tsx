2f53e6c5483b1ee120ab35b8b90b5e476e847240
antran1245 <antran1245@gmail.com>
2023-01-30 21:04:55 -0800

    style the dropdown for font and global font

--- pages/components/Setting.tsx
+++ pages/components/Setting.tsx
@@ -1,26 +1,36 @@
+import React from 'react'
+import Image from 'next/image'
 import logo from '../../assets/images/logo.svg'
 import moon from '../../assets/images/icon-moon.svg'
-import Image from 'next/image'
+import arrow from '../../assets/images/icon-arrow-down.svg'
 import styles from '../../styles/Setting.module.css'
-import React from 'react'
 
 type SettingProps = {
   mode: boolean;
   setMode: React.Dispatch<React.SetStateAction<boolean>>;
+  text: string;
+  setText: React.Dispatch<React.SetStateAction<string>>;
 }
 
-export default function Setting({ mode, setMode }: SettingProps) {
+export default function Setting({ mode, setMode, text, setText }: SettingProps) {
+
   return (
     <div className={styles.settingBar}>
       <Image src={logo} alt="logo" width={32} height={36.5} />
       <div className={styles.setting}>
         <div className={styles.dropdown}>
-          <select>
-            <option value="">Sans Serif</option>
-            <option value="">Serif</option>
-            <option value="">Mono</option>
-          </select>
-          <span>&#x2304;</span>
+          {/* <select value={text} onChange={(e) => setText(e.target.value)}>
+            <option value="sans-serif">Sans Serif</option>
+            <option value="serif">Serif</option>
+            <option value="mono">Mono</option>
+          </select> */}
+          <p>{text}</p>
+          <Image src={arrow} alt="arrow" width={12} height={6} />
+          <div className={styles.select}>
+            <p className={styles.sans} onClick={() => setText('Sans Serif')}>Sans Serif</p>
+            <p className={styles.serif} onClick={() => setText('Serif')}>Serif</p>
+            <p className={styles.mono} onClick={() => setText('Mono')}>Mono</p>
+          </div>
         </div>
         <span>&#x2502;</span>
         <div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,12 @@ import Setting from './components/Setting'
 
 export default function Home() {
   const [mode, setMode] = useState<boolean>(false)
+  const [text, setText] = useState<string>('Sans Serif')
   return (
-    <div className={styles.container} data-color-mode={`${mode ? 'dark' : 'light'}`}>
+    <div className={styles.container} data-color-mode={`${mode ? 'dark' : 'light'}`} text-mode={text || 'Sans Serif'}>
       <div>
         <Heading />
-        <Setting mode={mode} setMode={setMode} />
+        <Setting mode={mode} setMode={setMode} text={text} setText={setText} />
       </div>
     </div>
   )
