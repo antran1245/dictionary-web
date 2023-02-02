c12c1c0a6cd971eb0f90ed1e8dc9f4cf4e56a32b
antran1245 <antran1245@gmail.com>
2023-02-02 12:39:28 -0800

    styling of the search bar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 import { useState } from 'react'
+import Image from 'next/image'
 import Heading from './components/Heading'
-import styles from '../styles/Home.module.css'
 import Setting from './components/Setting'
+import magnifyingGlass from '../assets/images/icon-search.svg'
+import styles from '../styles/Home.module.css'
 
 export default function Home() {
   const [mode, setMode] = useState<boolean>(false)
@@ -11,6 +13,10 @@ export default function Home() {
       <div>
         <Heading />
         <Setting mode={mode} setMode={setMode} text={text} setText={setText} />
+        <div className={styles.searchBar}>
+          <input type={"text"} placeholder="Search for any word..." />
+          <Image src={magnifyingGlass} alt="magnifyingGlass" width={15.55} height={15.55} className={styles.magnifyingGlass} />
+        </div>
       </div>
     </div>
   )
