1abe2d7d6bf187af7bb94e487ce421af4e3a373d
antran1245 <antran1245@gmail.com>
2023-02-12 00:35:43 -0800

    fix a ` ' ` error to &apos;

--- pages/components/NotFound.tsx
+++ pages/components/NotFound.tsx
@@ -5,7 +5,7 @@ export default function NotFound() {
     <div className={styles.found}>
       <span>&#128533;</span>
       <h3>No Definitions Found</h3>
-      <p>Sorry pal, we couldn't find definitions for the word you were looking for. You can try the search again at later time or head to the web instead.</p>
+      <p>Sorry pal, we couldn&apos;t find definitions for the word you were looking for. You can try the search again at later time or head to the web instead.</p>
     </div>
   )
 }
